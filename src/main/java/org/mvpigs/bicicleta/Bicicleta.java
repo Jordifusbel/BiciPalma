cb7521acba1c807f46b8694c5bacd2eb512e45b7
Jordi <jordifusbel@hotmail.com>
2018-02-14 22:34:28 +0100

    variables y getters/setters añandidos

--- src/main/java/org/mvpigs/bicicleta/Bicicleta.java
+++ src/main/java/org/mvpigs/bicicleta/Bicicleta.java
@@ -1,4 +1,25 @@
 package org.mvpigs.bicicleta;
 
 public class Bicicleta {
+
+    private Boolean anclada;
+    private int id;
+
+    public Boolean getAnclada() {
+        return anclada;
+    }
+
+    public void setAnclada(Boolean anclada) {
+        this.anclada = anclada;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
 }
